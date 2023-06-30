76eea1f793d84f85a16e71ee1836f66fe787a755
nativegold <zxc6941@gmail.com>
2023-07-01 03:14:54 +0900

    [Modify] Modified structure

--- main.py
+++ main.py
@@ -1,83 +1,59 @@
-# 크루스칼 알고리즘 구현
+class Kruskal:
+    def __init__(self, graph, n):
+        self.graph = graph  # 입력 그래프를 인접 행렬로
+        self.n = n  # 정점의 개수
+        self.parent = [-1] * n  # 부모 리스트 초기화
+        self.mst_e = []  # MST에 포함된 간선을 저장하는 리스트
+        self.mst_weight = 0  # MST의 총 가중치
 
-# parent 전역 변수로 지정
-global parent
+    # 정점 u가 속한 집합의 루트를 찾는 메소드
+    def find(self, u):
+        if self.parent[u] >= 0:  # u가 속한 집합의 루트가 아닐 경우
+            self.parent[u] = self.find(self.parent[u])  # 재귀적으로 u의 루트 찾기
+            return self.parent[u]
+        else:  # u가 속한 집합의 루트일 경우
+            return u
 
-# parent 리스트를 정점 갯수 만큼 초기화
-def init_set(n):
-    global parent
-    parent = [-1] * n
+    # 정점 u와 v가 속한 집합을 합치는 메소드
+    def union(self, u, v):
+        u_root = self.find(u)
+        v_root = self.find(v)
+        if self.parent[u_root] <= self.parent[v_root]:
+            self.parent[u_root] += self.parent[v_root]
+            self.parent[v_root] = u_root
+        else:
+            self.parent[v_root] += self.parent[u_root]
+            self.parent[u_root] = v_root
 
+    # 크루스칼 알고리즘으로 그래프의 MST를 찾는 메소드
+    def kruskal_mst(self):
+        E = []  # 그래프의 모든 간선을 저장하는 리스트
+        edges = 0  # MST에 포함된 간선의 개수
+        for i in range(0, self.n):  # 인접 행렬을 순회하여 모든 간선 찾기
+            for j in range(0, self.n):
+                if self.graph[i][j] > 0:
+                    E.append((i, j, self.graph[i][j]))  # 간선 (i, j)와 가중치 self.graph[i][j]를 리스트 E에 추가
+                    self.graph[j][i] = 0  # 중복 간선 피하기
+        E.sort(key=lambda x: x[2])  # 간선들을 가중치의 오름차순으로 정렬
 
-# 가중치를 이용한 union 함수
-def union(u, v):
-    global parent
+        while edges < self.n - 1:  # MST에 (n - 1)개의 간선이 포함될 때까지
+            u, v, weight = E.pop(0)  # 가장 작은 가중치를 가진 간선 선택
+            if self.find(u) != self.find(v):  # u와 v가 다른 집합에 속해 있을 경우
+                self.union(u, v)  # u와 v가 속한 집합 합치기
+                self.mst_e.append((u, v))  # 간선 (u, v)를 MST에 추가
+                edges += 1  # MST에 포함된 간선의 개수 증가
+                self.mst_weight += weight  # 간선의 가중치를 MST의 총 가중치에 추가
+        self.print_results()  # 결과 출력
 
-    # 루트 탐색
-    u_root = find(u)
-    v_root = find(v)
-
-    # 가중치를 이용하여 union 연산
-    if parent[u_root] <= parent[v_root]:    # v_root
-        parent[u_root] += parent[v_root]
-        parent[v_root] = u_root
-    else:
-        parent[v_root] += parent[u_root]
-        parent[u_root] = v_root
-
-
-# 붕괴 법칙을 이용한 find 함수
-def find(u):
-    global parent
-    if parent[u] >= 0:  # 자신이 소속된 집합의 루트가 아닐 경우
-        parent[u] = find(parent[u])
-        return parent[u]
-    else:   # 자신이 소속된 집합의 루트일 경우
-        return u
-
-
-# 크루스칼 알고리즘
-def krusukal_MST(matrix, n: int):
-    global parent  # 전역 변수로 선언
-    E = []  # 비용인접행렬을 튜플 (u, v, 가중치) 변환하여 정렬하기 위한 리스트
-    MST_E = []  # MST에 선택된 간선 (u, v)을 추가하기 위한 리스트
-    edges = 0   # 간선 수
-    MST_weight = 0  # MST 가중치
-
-    # 비용인접행렬을 (u, v, 가중치) 형태로 E 리스트에 추가
-    for i in range(0, n):
-        for j in range(0, n):
-            if matrix[i][j] > 0:
-                E.append((i, j, matrix[i][j]))
-                matrix[j][i] = 0
-
-    # (u, v, 가중치) 간선의 가중치에 따라 오름차순으로 정렬
-    E.sort(key=lambda x: x[2])
-
-    # parent 리스트를 정점 개수 만큼 초기화
-    init_set(n)
-
-    # '크루스칼 알고리즘 MST의 간선의 개수(n - 1)'만큼 반복
-    while edges < n - 1:
-        u, v, weight = E.pop(0) # 리스트의 pop(0) 함수로 가중치가 작은 간선부터 가져오기 (u: 정점, v: 정점, weight: 가중치)
-
-        if find(u) != find(v):
-            union(u, v)  # 간선 u의 집합과 간선 v의 집합 union
-            MST_E.append((u, v))  # MST에 간선 추가
-            edges += 1  # MST의 간선 수 +1
-            MST_weight += weight    # MST의 가중치 합산
-
-    # 출력
-    print(f"parent: {parent}")
-    print(f"MST 선택한 간선: {MST_E}")
-    print(f"MST 가중치: {MST_weight}")
-
-    # END
+    # 알고리즘 실행 결과를 출력하는 메소드
+    def print_results(self):
+        print(f"Parent: {self.parent}")
+        print(f"MST에서 선택된 간선: {self.mst_e}")
+        print(f"MST의 총 가중치: {self.mst_weight}")
 
 
-# main
 if __name__ == '__main__':
-    krusukal_MST([  # 비용인접행렬, n
+    Kruskal([
         [0, 4, 0, 0, 0, 0, 0, 8, 0],
         [4, 0, 8, 0, 0, 0, 0, 11, 0],
         [0, 8, 0, 7, 0, 4, 0, 0, 2],
@@ -87,4 +63,4 @@ if __name__ == '__main__':
         [0, 0, 0, 0, 0, 2, 0, 1, 6],
         [8, 11, 0, 0, 0, 0, 1, 0, 7],
         [0, 0, 2, 0, 0, 0, 6, 7, 0]
-    ], 9)
+    ], 9).kruskal_MST()
